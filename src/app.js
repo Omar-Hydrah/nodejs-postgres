366ad1c227a174f16ce57e6e375d4b3c54a1d157
Omar <omaralsalahy@gmx.com>
2022-06-29 23:20:58 -0300

    Apply the user router and necessary middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const userRouter = require("./router/user_router");
+
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRouter);
 dotenv.config();
 app.listen(3000, async function () {
   console.log("Listening on port 3000");
